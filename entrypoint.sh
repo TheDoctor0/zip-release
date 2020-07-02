a1afcab9c664c9976ac398fa831eac67bed1eb0e
The Doctor <dawid.janik95@gmail.com>
2020-07-02 20:03:05 +0200

    Fix exclusions.

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,24 +7,24 @@ printf "\n📦 Creating %s archive...\n" "$5"
 
 if [ "$3" != "." ]
 then
-  cd "$3"
+  cd $3
 fi
 
 if [ "$5" = "zip" ]
 then
   if [ -z "$4" ]
   then
-    zip -r "$1" "$2" || { printf "\n⛔ Unable to create %s archive.\n" "$5"; exit 1;  }
+    zip -r $1 $2 || { printf "\n⛔ Unable to create %s archive.\n" "$5"; exit 1;  }
   else
-    zip -r "$1" "$2" -x "$4" || { printf "\n⛔ Unable to create %s archive.\n" "$5"; exit 1;  }
+    zip -r $1 $2 -x $4 || { printf "\n⛔ Unable to create %s archive.\n" "$5"; exit 1;  }
   fi
 elif [ "$5" = "tar" ]
 then
   if [ -z "$4" ]
   then
-    tar -zcvf "$1" "$2" || { printf "\n⛔ Unable to create %s archive.\n" "$5"; exit 1;  }
+    tar -zcvf $1 $2 || { printf "\n⛔ Unable to create %s archive.\n" "$5"; exit 1;  }
   else
-    tar -zcvf "$1" "$2" --exclude="$4" || { printf "\n⛔ Unable to create %s archive.\n" "$5"; exit 1;  }
+    tar -zcvf $1 $2 --exclude=$4 || { printf "\n⛔ Unable to create %s archive.\n" "$5"; exit 1;  }
   fi
 else
   printf "\n⛔ Invalid archiving tool.\n"; exit 1;

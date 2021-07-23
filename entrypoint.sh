1f0dd690f9907c92dedc218b5666c7c0107f2c99
Dawid Janik <dawid.janik@inception.dev>
2021-07-23 16:59:16 +0200

    Add Windows support (#19) - closes #17

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,44 @@
-#! /bin/sh
+#! /bin/bash
 
 # Create archive or exit if command fails
 set -eu
 
 printf "\n📦 Creating %s archive...\n" "$INPUT_TYPE"
 
-if [ "$INPUT_DIRECTORY" != "." ]
+if [ "$INPUT_DIRECTORY" != "." ] 
 then
   cd $INPUT_DIRECTORY
 fi
 
-if [ "$INPUT_TYPE" = "zip" ]
+if [ "$INPUT_TYPE" = "zip" ] 
 then
-  if [ -z "$INPUT_EXCLUSIONS" ]
+  if [ "$RUNNER_OS" = "Windows" ]
   then
-    zip -r $INPUT_FILENAME $INPUT_PATH || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  }
+    if [ -z "$INPUT_EXCLUSIONS" ] 
+    then
+      7z a -tzip -r $INPUT_FILENAME $INPUT_PATH || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  }
+    else
+      EXCLUSIONS=''
+
+      for EXCLUSION in $INPUT_EXCLUSIONS
+      do
+        EXCLUSIONS+=" -x!"
+        EXCLUSIONS+=$EXCLUSION
+      done
+
+      7z a -tzip -r $INPUT_FILENAME $INPUT_PATH $EXCLUSIONS || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  }
+    fi
   else
-    zip -r $INPUT_FILENAME $INPUT_PATH -x $INPUT_EXCLUSIONS || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  }
+    if [ -z "$INPUT_EXCLUSIONS" ] 
+    then
+      zip -r $INPUT_FILENAME $INPUT_PATH || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  }
+    else
+      zip -r $INPUT_FILENAME $INPUT_PATH -x $INPUT_EXCLUSIONS || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  }
+    fi
   fi
-elif [ "$INPUT_TYPE" = "tar" ]
+elif [ "$INPUT_TYPE" = "tar" ] 
 then
-  if [ -z "$INPUT_EXCLUSIONS" ]
+  if [ -z "$INPUT_EXCLUSIONS" ] 
   then
     tar -zcvf $INPUT_FILENAME $INPUT_PATH || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  }
   else

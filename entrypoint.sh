876d50c4ed4620fcc78b047d3ddb8b4d7c644795
DENIEL-VALERO Ewan <ewandenielvalero@orange.fr>
2023-05-28 04:27:49 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -64,6 +64,22 @@ then
 
     7z a -tzip $INPUT_FILENAME $INPUT_PATH $EXCLUSIONS $INPUT_CUSTOM || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  }
   fi
+elif [ "$INPUT_TYPE" = "tar" ] || [ "$INPUT_TYPE" = "tar.gz" ] 
+then
+  if [ -z "$INPUT_EXCLUSIONS" ] 
+  then
+    tar -zcvf $INPUT_FILENAME $INPUT_PATH $INPUT_CUSTOM || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  }
+  else
+    EXCLUSIONS=''
+
+    for EXCLUSION in $INPUT_EXCLUSIONS
+    do
+      EXCLUSIONS+=" --exclude="
+      EXCLUSIONS+=$EXCLUSION
+    done
+
+    tar $EXCLUSIONS -zcvf $INPUT_FILENAME $INPUT_PATH $INPUT_CUSTOM || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  }
+  fi
 else
   printf "\n⛔ Invalid archiving tool.\n"; exit 1;
 fi

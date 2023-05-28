f222d88eb488e4e1e6538bcbdd0b8f1ff6aa6e90
DENIEL-VALERO Ewan <ewandenielvalero@orange.fr>
2023-05-28 04:25:19 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -45,40 +45,24 @@ then
 elif [ "$INPUT_TYPE" = "7z" ] || [ "$INPUT_TYPE" = "7zip" ] 
 then
   if [ -z "$INPUT_EXCLUSIONS" ] 
-    then
-      7z a -tzip $INPUT_FILENAME $INPUT_PATH $INPUT_CUSTOM || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  }
-    else
-      EXCLUSIONS=''
-
-      for EXCLUSION in $INPUT_EXCLUSIONS
-      do
-        EXCLUSIONS+=" -x!"
-        EXCLUSIONS+=$EXCLUSION
-      done
-
-      for EXCLUSION in $INPUT_RECURSIVE_EXCLUSIONS
-      do
-        EXCLUSIONS+=" -xr!"
-        EXCLUSIONS+=$EXCLUSION
-      done
-
-      7z a -tzip $INPUT_FILENAME $INPUT_PATH $EXCLUSIONS $INPUT_CUSTOM || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  }
-    fi
-elif [ "$INPUT_TYPE" = "tar" ] || [ "$INPUT_TYPE" = "tar.gz" ] 
-then
-  if [ -z "$INPUT_EXCLUSIONS" ] 
   then
-    tar -zcvf $INPUT_FILENAME $INPUT_PATH $INPUT_CUSTOM || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  }
+    7z a -tzip $INPUT_FILENAME $INPUT_PATH $INPUT_CUSTOM || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  }
   else
     EXCLUSIONS=''
 
     for EXCLUSION in $INPUT_EXCLUSIONS
     do
-      EXCLUSIONS+=" --exclude="
+      EXCLUSIONS+=" -x!"
       EXCLUSIONS+=$EXCLUSION
     done
 
-    tar $EXCLUSIONS -zcvf $INPUT_FILENAME $INPUT_PATH $INPUT_CUSTOM || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  }
+    for EXCLUSION in $INPUT_RECURSIVE_EXCLUSIONS
+    do
+      EXCLUSIONS+=" -xr!"
+      EXCLUSIONS+=$EXCLUSION
+    done
+
+    7z a -tzip $INPUT_FILENAME $INPUT_PATH $EXCLUSIONS $INPUT_CUSTOM || { printf "\n⛔ Unable to create %s archive.\n" "$INPUT_TYPE"; exit 1;  }
   fi
 else
   printf "\n⛔ Invalid archiving tool.\n"; exit 1;
